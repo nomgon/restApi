cf1592565dd1cd471a6a228559ded83ac254f49a
nomgon <batjargal.gochoosuren.999@gmail.com>
2023-10-14 23:53:48 +0800

    custom drawerMenu-г хийлээ

--- middleware/protect.js
+++ middleware/protect.js
@@ -4,15 +4,13 @@ const MyError = require("../utils/myError");
 const User = require("../models/User");
 
 exports.protect = asyncHandler(async (req, res, next) => {
-  // console.log(req.headers);
   let token = null;
-
   if (req.headers.authorization) {
     token = req.headers.authorization.split(" ")[1];
-  } else if (req.cookies) {
+  } 
+  else if (req.cookies) {
     token = req.cookies["amazon-token"];
   }
-
   if (!token) {
     throw new MyError(
       "이 작업을 수행할 권한이 없습니다. 먼저 로그인을 해주세요. Authorization 헤더를 통해 또는 쿠키를 사용하여 토큰을 전달합니다.",
